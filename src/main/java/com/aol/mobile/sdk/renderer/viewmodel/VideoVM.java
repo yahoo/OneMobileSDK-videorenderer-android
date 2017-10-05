98dcbf95c570703ba3250036fb460132d9ff34c9
Oleg_Kozhemiakin <o.kozhemiakin@gmail.com>
2017-10-05 16:06:01 +0300

    OMSDK-302 Chromecast implementation

--- src/main/java/com/aol/mobile/sdk/renderer/viewmodel/VideoVM.java
+++ src/main/java/com/aol/mobile/sdk/renderer/viewmodel/VideoVM.java
@@ -16,17 +16,22 @@ public class VideoVM {
     @Nullable
     public String videoUrl;
     @Nullable
+    public String title;
+    @Nullable
     public String subtitleLang;
     @Nullable
     public String subtitleUrl;
     @Nullable
     public Long seekPosition;
+    @Nullable
+    public Long currentPosition;
     public boolean isScalable = true;
     public boolean isMaintainAspectRatio = true;
     public boolean isMuted;
     public boolean shouldPlay;
     public double longitude;
     public double latitude;
+    public boolean isLive;
     @Nullable
     public Callbacks callbacks;
 
