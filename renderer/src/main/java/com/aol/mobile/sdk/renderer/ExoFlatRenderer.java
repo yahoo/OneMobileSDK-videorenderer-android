b560b681a7af3163193ec444648dcc600c3a3e88
AleksanderLevadniy <aleksander.levadniy@oath.com>
2018-08-01 20:35:30 +0300

    Chromecast turn off

--- renderer/src/main/java/com/aol/mobile/sdk/renderer/ExoFlatRenderer.java
+++ renderer/src/main/java/com/aol/mobile/sdk/renderer/ExoFlatRenderer.java
@@ -10,26 +10,19 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.aol.mobile.sdk.annotations.PublicApi;
-import com.aol.mobile.sdk.chromecast.CastRenderer;
-import com.aol.mobile.sdk.chromecast.CastRendererImpl;
-import com.aol.mobile.sdk.chromecast.OneCastManager;
 import com.aol.mobile.sdk.renderer.internal.FlatRendererView;
-import com.aol.mobile.sdk.renderer.viewmodel.CastVideoVMTranslator;
 import com.aol.mobile.sdk.renderer.viewmodel.VideoVM;
-
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 @PublicApi
 public final class ExoFlatRenderer extends ExoVideoRenderer {
-    private CastRenderer castRenderer;
-    private CastVideoVMTranslator translator;
+//    private CastRenderer castRenderer;
+//    private CastVideoVMTranslator translator;
     private Context context;
     private boolean isChromecastModulePresent;
-    private OneCastManager oneCastManager;
+//    private OneCastManager oneCastManager;
 
     public ExoFlatRenderer(@NonNull Context context) {
         super(context);
@@ -37,11 +30,11 @@ public final class ExoFlatRenderer extends ExoVideoRenderer {
         setRenderer(flatRendererView);
         this.context = context;
 
-        checkChromecastModulePresence();
-
-        if (isChromecastModulePresent) {
-            oneCastManager = new OneCastManager(context);
-        }
+//        checkChromecastModulePresence();
+//
+//        if (isChromecastModulePresent) {
+//            oneCastManager = new OneCastManager(context);
+//        }
     }
 
     private void checkChromecastModulePresence() {
@@ -55,62 +48,62 @@ public final class ExoFlatRenderer extends ExoVideoRenderer {
     }
 
     public void render(@NonNull VideoVM videoVM) {
-        if (videoVM.isCasting) {
-            showCastView();
-        } else {
-            showFlatView(videoVM);
-        }
+//        if (videoVM.isCasting) {
+//            showCastView();
+//        } else {
+//            showFlatView(videoVM);
+//        }
 
-        if (castRenderer != null && translator != null) {
-            castRenderer.render(translator.translate(videoVM));
-            videoVM.shouldPlay = false;
-        }
+//        if (castRenderer != null && translator != null) {
+//            castRenderer.render(translator.translate(videoVM));
+//            videoVM.shouldPlay = false;
+//        }
         super.render(videoVM);
     }
 
-    private void showCastView() {
-        if (castRenderer == null) {
-            castRenderer = getCastRenderer();
-            translator = new CastVideoVMTranslator();
-            if (castRenderer != null) {
-                addView(castRenderer.getViewport(), MATCH_PARENT, MATCH_PARENT);
-            }
-        }
-    }
+//    private void showCastView() {
+//        if (castRenderer == null) {
+//            castRenderer = getCastRenderer();
+//            translator = new CastVideoVMTranslator();
+//            if (castRenderer != null) {
+//                addView(castRenderer.getViewport(), MATCH_PARENT, MATCH_PARENT);
+//            }
+//        }
+//    }
+//
+//    private void showFlatView(@NonNull VideoVM videoVM) {
+//        if (castRenderer != null) {
+//            removeView(castRenderer.getViewport());
+//            if (videoVM.currentPosition != null) {
+//                videoVM.seekPosition = videoVM.currentPosition;
+//            }
+//            castRenderer = null;
+//            stopCasting();
+//        }
+//    }
 
-    private void showFlatView(@NonNull VideoVM videoVM) {
-        if (castRenderer != null) {
-            removeView(castRenderer.getViewport());
-            if (videoVM.currentPosition != null) {
-                videoVM.seekPosition = videoVM.currentPosition;
-            }
-            castRenderer = null;
-            stopCasting();
-        }
-    }
-
-    @Nullable
-    private CastRenderer getCastRenderer() {
-        return new CastRendererImpl(context);
-    }
+//    @Nullable
+//    private CastRenderer getCastRenderer() {
+//        return new CastRendererImpl(context);
+//    }
 
     @Override
     public void dispose() {
         super.dispose();
-        if (castRenderer != null) {
-            stopCasting();
-        }
+//        if (castRenderer != null) {
+//            stopCasting();
+//        }
     }
 
-    private void stopCasting() {
-        if (!isChromecastModulePresent)
-            return;
-
-        getHandler().post(new Runnable() {
-            @Override
-            public void run() {
-                oneCastManager.stopCasting();
-            }
-        });
-    }
+//    private void stopCasting() {
+//        if (!isChromecastModulePresent)
+//            return;
+//
+//        getHandler().post(new Runnable() {
+//            @Override
+//            public void run() {
+//                oneCastManager.stopCasting();
+//            }
+//        });
+//    }
 }
--- renderer/src/main/java/com/aol/mobile/sdk/renderer/viewmodel/CastVideoVMTranslator.java
+++ renderer/src/main/java/com/aol/mobile/sdk/renderer/viewmodel/CastVideoVMTranslator.java
@@ -5,57 +5,55 @@
 
 package com.aol.mobile.sdk.renderer.viewmodel;
 
-import com.aol.mobile.sdk.chromecast.CastVideoVM;
-
 public class CastVideoVMTranslator {
-    private CastVideoVM castVideoVM = new CastVideoVM();
-    private VideoVM.Callbacks callbacks;
-
-    public CastVideoVM translate(final VideoVM videoVM) {
-        if (castVideoVM.callbacks == null) {
-            castVideoVM.callbacks = new CastVideoVM.Callbacks() {
-                @Override
-                public void onDurationReceived(long duration) {
-                    if (callbacks == null) return;
-                    callbacks.onDurationReceived(duration);
-                }
-
-                @Override
-                public void onVideoPositionUpdated(long position) {
-                    if (callbacks == null) return;
-                    callbacks.onVideoPositionUpdated(position);
-                }
-
-                @Override
-                public void onVideoPlaybackFlagUpdated(boolean isActuallyPlaying) {
-                    if (callbacks == null) return;
-                    callbacks.onVideoPlaybackFlagUpdated(isActuallyPlaying);
-                }
-
-                @Override
-                public void onVideoEnded() {
-                    if (callbacks == null) return;
-                    callbacks.onVideoEnded();
-                }
-
-                @Override
-                public void onSeekPerformed() {
-                    if (callbacks == null) return;
-                    callbacks.onSeekPerformed();
-                }
-            };
-        }
-        castVideoVM.videoUrl = videoVM.videoUrl;
-        castVideoVM.title = videoVM.title;
-        castVideoVM.seekPosition = videoVM.seekPosition;
-        castVideoVM.currentPosition = videoVM.currentPosition;
-        castVideoVM.isMuted = videoVM.isMuted;
-        castVideoVM.shouldPlay = videoVM.shouldPlay;
-        castVideoVM.isLive = videoVM.isLive;
-        castVideoVM.isActive = videoVM.isActive;
-        castVideoVM.isCasting = videoVM.isCasting;
-        castVideoVM.isAd = videoVM.isAd;
-        callbacks = videoVM.callbacks;
-        return castVideoVM;
-    }
+//    private CastVideoVM castVideoVM = new CastVideoVM();
+//    private VideoVM.Callbacks callbacks;
+//
+//    public CastVideoVM translate(final VideoVM videoVM) {
+//        if (castVideoVM.callbacks == null) {
+//            castVideoVM.callbacks = new CastVideoVM.Callbacks() {
+//                @Override
+//                public void onDurationReceived(long duration) {
+//                    if (callbacks == null) return;
+//                    callbacks.onDurationReceived(duration);
+//                }
+//
+//                @Override
+//                public void onVideoPositionUpdated(long position) {
+//                    if (callbacks == null) return;
+//                    callbacks.onVideoPositionUpdated(position);
+//                }
+//
+//                @Override
+//                public void onVideoPlaybackFlagUpdated(boolean isActuallyPlaying) {
+//                    if (callbacks == null) return;
+//                    callbacks.onVideoPlaybackFlagUpdated(isActuallyPlaying);
+//                }
+//
+//                @Override
+//                public void onVideoEnded() {
+//                    if (callbacks == null) return;
+//                    callbacks.onVideoEnded();
+//                }
+//
+//                @Override
+//                public void onSeekPerformed() {
+//                    if (callbacks == null) return;
+//                    callbacks.onSeekPerformed();
+//                }
+//            };
+//        }
+//        castVideoVM.videoUrl = videoVM.videoUrl;
+//        castVideoVM.title = videoVM.title;
+//        castVideoVM.seekPosition = videoVM.seekPosition;
+//        castVideoVM.currentPosition = videoVM.currentPosition;
+//        castVideoVM.isMuted = videoVM.isMuted;
+//        castVideoVM.shouldPlay = videoVM.shouldPlay;
+//        castVideoVM.isLive = videoVM.isLive;
+//        castVideoVM.isActive = videoVM.isActive;
+//        castVideoVM.isCasting = videoVM.isCasting;
+//        castVideoVM.isAd = videoVM.isAd;
+//        callbacks = videoVM.callbacks;
+//        return castVideoVM;
+//    }
 }
